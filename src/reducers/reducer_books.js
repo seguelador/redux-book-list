e5fb390cd5876d671b3295cb2b0ccfdc51b8620a
Matias Seguel <matias.seguel.estay@gmail.com>
2017-12-24 01:04:41 -0300

    Finished redux section

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript: THe Good Parts', pages: 101 },
+    { title: 'Harry Potter', pages: 20 },
+    { title: 'The Dark Tower', pages: 500 },
+    { title: 'Eloquent RUby', pages: 1 },
+  ]
+}
